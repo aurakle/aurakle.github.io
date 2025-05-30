b956c754f685fa7ec779956d27135bd09a917fbc
Aurora Dawn <aurora.dawn@trans.fish>
2025-05-29 20:21:05 -0400

    Seems good

--- docs/js/wobbly-text.js
+++ docs/js/wobbly-text.js
@@ -9,22 +9,25 @@ elements.forEach((element) => {
     }
 })
 
-function nestedSpans(inner, depth) {
+function nestedSpans(text, depth) {
+    let current = text;
+
     for (let i = 0; i < Math.floor(Math.abs(depth)) + 1; i++) {
         let span = document.createElement("span");
 
-        if (typeof inner === "string") {
-            span.innerText = inner;
+        if (typeof current === "string") {
+            span.innerText = current;
         } else {
-            span.appendChild(inner);
+            span.appendChild(current);
         }
 
         span.style.display = "inline-block";
         span.style.animation = `${Math.random() * 0.4}s spin linear infinite ${-Math.random() * 0.2}s alternate`;
         span.style.transform = `translate(${(Math.random() * 2 - 1) * 0.08}em, ${(Math.random() * 2 - 1) * 0.1}em)`;
 
-        inner = span;
+        current = span;
     }
 
-    return inner;
+    current.ariaLabel = text;
+    return current;
 }
